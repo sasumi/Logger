2bc34758f7254bb9a8f13ddcf688fa5223c4e71c
sasumi <cnsasumi@outlook.com>
2020-04-01 18:44:01 +0800

    console support colorless

--- src/Output/ConsoleOutput.php
+++ src/Output/ConsoleOutput.php
@@ -6,21 +6,31 @@ use LFPhp\Logger\Logger;
 use LFPhp\Logger\LoggerLevel;
 use function LFPhp\Func\console_color;
 
-/**
- * 控制台（命令行模式）输出
- */
 class ConsoleOutput extends CommonAbstract {
+	private $colorless = false;
+
 	public static $level_colors = [
 		LoggerLevel::DEBUG     => ['dark_gray'],
-		LoggerLevel::INFO      => ['green'],
+		LoggerLevel::INFO      => ['white'],
 		LoggerLevel::WARNING   => ['yellow'],
 		LoggerLevel::ERROR     => ['red'],
 		LoggerLevel::CRITICAL  => ['purple'],
 		LoggerLevel::EMERGENCY => ['cyan'],
 	];
 
+	/**
+	 * ConsoleOutput constructor.
+	 * @param bool $colorless
+	 */
+	public function __construct($colorless = false){
+		$this->colorless = $colorless;
+	}
+
 	public function output($messages, $level, $logger_id, $trace_info = null){
-		$lv_str = console_color(strtoupper($level), self::$level_colors[$level][0], self::$level_colors[$level][1]);
+		$lv_str = strtoupper($level);
+		if(!$this->colorless){
+			$lv_str = console_color($lv_str, self::$level_colors[$level][0], self::$level_colors[$level][1]);
+		}
 		echo date('H:i:s m/d'), ($trace_info ? '' : ' '.$logger_id).' - ', $lv_str.' - ', Logger::combineMessages($messages);
 		if($trace_info){
 			echo ' ';
